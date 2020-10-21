990e13628520478ad0b18a3252be882f8e38c375
kastrupf <kastrupf@gmail.com>
2020-10-21 17:08:06 +0200

    configurations initiales

--- src/main/java/com/kastrupf/osworks/api/controller/clientController.java
+++ src/main/java/com/kastrupf/osworks/api/controller/clientController.java
@@ -1,14 +1,33 @@
 package com.kastrupf.osworks.api.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kastrupf.osworks.domain.model.Client;
+
 @RestController
-public class clientController {
+public class ClientController {
 	
 	@GetMapping("/clients")
-	public String list() {
-		return "Test";
+	public List<Client> lister() {
+		var client1 = new Client();
+		client1.setId(1L);
+		client1.setNom("MENDES");
+		client1.setPrenom("Juan");
+		client1.setEmail("juan@email.com");
+		client1.setTelephone("05.66.77.88.99");
+		
+		var client2= new Client();
+		client2.setId(1L);
+		client2.setNom("ALMEIDA");
+		client2.setPrenom("Joana");
+		client2.setEmail("joana@email.com");
+		client2.setTelephone("06.88.77.66.55");
+		
+		return Arrays.asList(client1, client2);
 	}
 
 }
---
+++ src/main/java/com/kastrupf/osworks/domain/model/Client.java
@@ -0,0 +1,43 @@
+package com.kastrupf.osworks.domain.model;
+
+public class Client {
+	
+	private Long id;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String telephone;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	
+	
+	
+}
