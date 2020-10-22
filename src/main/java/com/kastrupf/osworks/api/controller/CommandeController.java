f0e667a0b2511a8d0c5f092da96b140c570d8426
kastrupf <kastrupf@gmail.com>
2020-10-22 17:00:59 +0200

    inclusion de la méthode pour lister toutes les commandes

--- src/main/java/com/kastrupf/osworks/api/controller/CommandeController.java
+++ src/main/java/com/kastrupf/osworks/api/controller/CommandeController.java
@@ -1,16 +1,21 @@
 package com.kastrupf.osworks.api.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kastrupf.osworks.domain.model.Client;
 import com.kastrupf.osworks.domain.model.Commande;
+import com.kastrupf.osworks.domain.repository.CommandeRepository;
 import com.kastrupf.osworks.domain.service.GestionCommande;
 
 @RestController
@@ -20,9 +25,20 @@ public class CommandeController {
 	@Autowired
 	private GestionCommande gestionCommande;
 	
+	@Autowired
+	private CommandeRepository commandeRepository;
+	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Commande creer(@Valid @RequestBody Commande commande) {
 		return gestionCommande.creer(commande);
 	}
+	
+	@GetMapping
+	public List<Commande> lister() {
+		return commandeRepository.findAll();	
+	}
+	
+	
+	
 }
