5445fa784852d78de6ea92346e143c9fa75166af
kastrupf <kastrupf@gmail.com>
2020-10-22 17:07:29 +0200

    recherche de commande par id

--- src/main/java/com/kastrupf/osworks/api/controller/CommandeController.java
+++ src/main/java/com/kastrupf/osworks/api/controller/CommandeController.java
@@ -1,12 +1,15 @@
 package com.kastrupf.osworks.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +41,19 @@ public class CommandeController {
 	public List<Commande> lister() {
 		return commandeRepository.findAll();	
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Commande> listerParId(@PathVariable Long id) {
+		Optional<Commande> commande= commandeRepository.findById(id);	
+		
+		if (commande.isPresent()) {
+			return ResponseEntity.ok(commande.get());
+		}
+					
+		return ResponseEntity.notFound().build();
+	}
+	
+	
 	
 	
 	
